103e0cd12c6da5319793d54efaca217563e2d852
Nathan Ell <huskerell13@hotmail.com>
2020-01-17 12:13:33 -0600

    Texture and Music
    Changed player ball texture and added background music.

---
+++ Assets/ItemPickUp.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemPickUp : MonoBehaviour
+{
+    public AudioSource itemSource;
+
+    //Use this for initialization
+    private void Start() {
+        itemSource = GetComponent<AudioSource>();
+    }
+
+    //Update is called once per frame
+    void Update ()  {
+
+    }
+
+    private void OnCollisionEnter(Collision collision) {
+        if (collision.gameObject.tag == "Target Object") {
+
+            itemSource.Play ();
+
+            Destroy(collision.gameObject);
+        }
+    }
+}
---
+++ Assets/ObjectPickUp.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectPickUp : MonoBehaviour
+{
+    public AudioSource tickSource;
+
+    //Use this for initialization
+    void Start()
+    {
+        tickSource = GetComponent<AudioSource> ();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Target Object")
+        {
+            tickSource.Play ();
+        }
+    }
+}
