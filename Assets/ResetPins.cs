c5fbc1686598778883d3b7a5de3d0715af681159
Ethan Butt <ethanbutt309@gmail.com>
2020-01-24 17:22:02 -0600

    Setup for resetting pins.

---
+++ Assets/ResetPins.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResetPins : MonoBehaviour
+{
+    public bool shouldReset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        shouldReset = false;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            shouldReset = true;
+        }
+    }
+}
