9cafc480b1d9ea3ca58a0387d1321aa0b745b457
Ethan Butt <ethanbutt309@gmail.com>
2020-01-29 13:21:25 -0600

    Setup for self-resetting pins.

---
+++ Assets/Scripts/ResetPins.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class ResetPins : MonoBehaviour
+{
+    private GameObject[] pins;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        pins = GameObject.FindGameObjectsWithTag("Pin");
+        print(pins);
+        print(pins.Length);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        print("Triggerred");
+        if (other.gameObject.CompareTag("Ball"))
+        {
+            foreach(GameObject pin in pins)
+            {
+                print(pin);
+                pin.GetComponent<Resetable>().ToggleReset(true);
+            }
+        }
+    }
+}
--- Assets/Scripts/SnapToPoint.cs
+++ Assets/Scripts/SnapToPoint.cs
@@ -5,20 +5,28 @@ using UnityEngine;
 
 namespace Valve.VR.InteractionSystem.Sample
 {
-    public class SnapToPoint : MonoBehaviour
+    public class SnapToPoint : MonoBehaviour, Resetable
     {
-        public GameObject resetButton;
-
-        private Transform snapTo;
+        private bool shouldReset;
+        private Vector3 snapTo;
+        private Quaternion snapAngle;
         private Rigidbody body;
         public float snapTime = 2;
 
         private float dropTimer;
         private Interactable interactable;
 
+        public void ToggleReset(bool status)
+        {
+            print(status);
+            shouldReset = status;
+        }
+
         private void Start()
         {
-            snapTo = transform;
+            snapTo = transform.TransformDirection(transform.position);
+            snapAngle = transform.rotation;
+            shouldReset = false;
             interactable = GetComponent<Interactable>();
             body = GetComponent<Rigidbody>();
         }
@@ -31,21 +39,26 @@ namespace Valve.VR.InteractionSystem.Sample
 
             if (used)
             {
+                print('1');
                 body.isKinematic = false;
                 dropTimer = -1;
             }
-            else if (resetButton.shouldReset)
+            else if (shouldReset)
             {
+                print('2');
                 dropTimer += Time.deltaTime / (snapTime / 2);
 
                 body.isKinematic = dropTimer > 1;
 
+                // Already back in target position
                 if (dropTimer > 1)
                 {
                     //transform.parent = snapTo;
-                    transform.position = snapTo.position;
-                    transform.rotation = snapTo.rotation;
+                    transform.position = snapTo;
+                    transform.rotation = snapAngle;
+                    shouldReset = false;
                 }
+                // Still returning to target position
                 else
                 {
                     float t = Mathf.Pow(35, dropTimer);
@@ -54,8 +67,8 @@ namespace Valve.VR.InteractionSystem.Sample
                     if (body.useGravity)
                         body.AddForce(-Physics.gravity);
 
-                    transform.position = Vector3.Lerp(transform.position, snapTo.position, Time.fixedDeltaTime * t * 3);
-                    transform.rotation = Quaternion.Slerp(transform.rotation, snapTo.rotation, Time.fixedDeltaTime * t * 2);
+                    transform.position = Vector3.Lerp(transform.position, snapTo, Time.fixedDeltaTime * t * 3);
+                    transform.rotation = Quaternion.Slerp(transform.rotation, snapAngle, Time.fixedDeltaTime * t * 2);
                 }
             }
         }
